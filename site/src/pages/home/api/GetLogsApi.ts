f0e743c2a76c8e993c5cb33bec7fb5301e2a418d
Andre Pereira <deco9871@gmail.com>
2023-04-25 16:52:01 -0300

    fix: github api limit

--- site/src/pages/home/api/GetLogsApi.ts
+++ site/src/pages/home/api/GetLogsApi.ts
@@ -4,7 +4,7 @@ export async function GetLogsApi() {
         headers: {Authorization: "b0e435db3ce961db66e3967b776b7b197a1ca334"}
     }
 
-    let response = await fetch("https://api.github.com/repos/insper-classroom/devlife-23-1-projeto-pygame-ian-e-andre/commits", options)
+    let response = await fetch("https://api.github.com/repos/insper-classroom/devlife-23-1-projeto-pygame-ian-e-andre/commits?per_page=100", options)
     let data = await response.json()
 
     return data
